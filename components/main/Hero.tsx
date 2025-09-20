791a9e6b92419370cf6262a735da164f5e580d07
Jake6654 <ugw2ad@virginia.edu>
2025-09-20 18:25:05 -0400

    Add Hero and HeroContent

---
+++ components/main/Hero.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import HeroContent from "../sub/HeroContent";
+const Hero = () => {
+  return (
+    // 부모의 height 을 명시적으로 보여줘야지 자식도 부모의 높이를 따라감 otherwise, h = 0px
+    <div className="relative flex flex-col h-full w-full">
+      {" "}
+      <video
+        autoPlay
+        muted
+        loop
+        className="rotate-180 absolute top-[-340px] left-0-z-[1] w-full h-full object-cover"
+      >
+        {" "}
+        <source src="/blackhole.webm" type="video/webm" />{" "}
+      </video>{" "}
+      <HeroContent />
+    </div>
+  );
+};
+export default Hero;
