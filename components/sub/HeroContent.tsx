a193a9fa23e99f4daeea5257942bd56900f65e51
Jake6654 <ugw2ad@virginia.edu>
2025-09-20 18:50:29 -0400

    finalized the here section

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -2,8 +2,13 @@
 
 import React from "react";
 import { motion } from "framer-motion";
-import { slideInFromTop } from "@/utils/motion";
+import {
+  slideInFromLeft,
+  slideInFromRight,
+  slideInFromTop,
+} from "@/utils/motion";
 import { SparklesIcon } from "@heroicons/react/24/outline";
+import Image from "next/image";
 
 const HeroContent = () => {
   return (
@@ -15,7 +20,7 @@ const HeroContent = () => {
       <div className="h-full w-full flex flex-col gap-5 justify-center m-auto text-start">
         <motion.div
           variants={slideInFromTop}
-          className="Welcome-box py-[8px] px-[4px] border border-[#7042f88b] opacity-[0.9] "
+          className="Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9] "
         >
           <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
           {/* Welcome-text is custom class*/}
@@ -23,7 +28,46 @@ const HeroContent = () => {
             Fullstack Developer Portfolio
           </h1>
         </motion.div>
+
+        <motion.div
+          variants={slideInFromLeft(0.5)}
+          className="flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto"
+        >
+          <span>
+            Providing
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
+              {" "}
+              the best{" "}
+            </span>
+            project expreience
+          </span>
+        </motion.div>
+        <motion.p
+          variants={slideInFromLeft(0.8)}
+          className="text-lg text-gray-400 my-5 max-w-[600px]"
+        >
+          Hi I&apos;m Jae-Hyuk! I&apos;m a Full Stack Software Engineer with a
+          strong interest in cloud computing and reponsive web design. Check out
+          my projects and skills.
+        </motion.p>
+        <motion.a
+          variants={slideInFromLeft(1)}
+          className="py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px] "
+        >
+          Learn More!
+        </motion.a>
       </div>
+      <motion.div
+        variants={slideInFromRight(0.8)}
+        className="w-full h-full felx justify-center items-center"
+      >
+        <Image
+          src={"/mainIconsdark.svg"}
+          alt="work icons"
+          height={650}
+          width={650}
+        />
+      </motion.div>
     </motion.div>
   );
 };
